cc51a1ce3a4ee0047952f6f5b49bc8aa5da8ce96
juliano988 <julio-pinheiroo@hotmail.com>
2020-11-26 19:19:45 -0300

    First day of project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const app = express();
 
 // Basic Configuration
@@ -10,15 +11,58 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// Initializing db
+const mongoose = require('mongoose');
+mongoose.connect(process.env.URI, { useNewUrlParser: true , useUnifiedTopology: true });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    const shortenSchema = new mongoose.Schema({
+        original_url: String,
+        short_url: String
+    });
+    const Links = mongoose.model('Links', shortenSchema);
+
+    // db functions
+    function saveLinks(org,shr) {
+        const link = new Links({
+            original_url: org,
+            short_url: shr
+        });
+        link.save(function(err,data){
+            if(err){return console.log(err)}
+            console.log(data);
+        });
+    };
+
+    // server functions
+    app.post('/api/shorturl/new',function(req,res){
+      if((/^(http|https):\/\/.+/).test(req.body.link)){
+        const randomCode = Math.round(Math.random()*99999).toString(10).padStart(5,"0");
+        const original = req.body.link;
+        const short = req.headers.host+'/'+randomCode;
+        saveLinks(original,short);
+        res.json({ original_url : original, short_url : short});
+      }
+      else{
+        res.json({ error: 'invalid url' });
+      }
+    });
+
+    //Listening server...
+    app.listen(port, function() {
+      console.log(`Listening on port ${port}`);
+    });
+
 });
